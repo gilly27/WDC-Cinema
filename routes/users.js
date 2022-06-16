1be037a8689bfba83f128e9b221f7ab859473308
gilly27 <87750095+gilly27@users.noreply.github.com>
2022-06-16 09:34:59 +0000

    Thu, Jun 16, 2022, 9:34 AM +00:00

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/*', function(req, res, next) {
+  if(req.session["user"])
+  {
+    next();
+  }
+  else
+  {
+    res.redirect('/landing');
+  }
 });
+
+router.get('/logout', function(req, res, next) {
+  req.session.destroy();
+});
+
 
 module.exports = router;
